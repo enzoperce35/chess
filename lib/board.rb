fc40cf9cad6eafc92f440f12588eed0ff7fb4dd5
enzoperce35 <enzoperce35@gmail.com>
2020-12-21 18:45:38 +0800

    added create_squares method

--- lib/board.rb
+++ lib/board.rb
@@ -1,25 +1,23 @@
 require_relative 'module.rb'
 
 class Board
-  attr_accessor :board
+  attr_accessor :squares
   include ChessParts
 
   def initialize
-    @board = draw_board
+    @squares = create_squares
   end
 
-  def draw_board
-    puts UPPER_LEFT + HORIZONTALS + UPPER_RIGHT,
-         '8 ' + VERTICAL + ((CYAN + BLACK) * 4) + VERTICAL,
-         '7 ' + VERTICAL + ((BLACK + CYAN) * 4) + VERTICAL,
-         '6 ' + VERTICAL + ((CYAN + BLACK) * 4) + VERTICAL,
-         '5 ' + VERTICAL + ((BLACK + CYAN) * 4) + VERTICAL,
-         '4 ' + VERTICAL + ((CYAN + BLACK) * 4) + VERTICAL,
-         '3 ' + VERTICAL + ((BLACK + CYAN) * 4) + VERTICAL,
-         '2 ' + VERTICAL + ((CYAN + BLACK) * 4) + VERTICAL,
-         '1 ' + VERTICAL + ((BLACK + CYAN) * 4) + VERTICAL,
-         LOWER_LEFT + HORIZONTALS + LOWER_RIGHT,
-         COLUMN_LETTERS
-  end
+  def create_squares(square_hash = {}, num = ('1'..'8').to_a, alpha = ('A'..'H').to_a)
+    return square_hash if num.length < 1
 
+    number = num.pop
+    8.times do |i|
+      color = i.odd? ? BLACK : CYAN
+
+      square_hash.store("square#{alpha[i]}#{number}", color)
+    end
+
+    create_squares(square_hash, num)
+  end
 end
