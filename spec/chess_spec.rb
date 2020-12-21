6b3aa4425519dcbb3a94d9ae103775f248602ada
enzoperce35 <enzoperce35@gmail.com>
2020-12-22 07:50:02 +0800

    restored

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -1,17 +1,13 @@
+require './lib/chess.rb'
 require './lib/board.rb'
 
-describe Board do
-  subject(:board) { described_class.new }
+describe Chess do
+  subject(:chess) { described_class.new }
 
   describe "#initialize" do
-    it "squares" do
-      expect(board.squares).to include("squareA6"=>"\e[0;39;46m    \e[0m")
+    it 'board' do
+      chess.board.squares = 'test'
+      expect(chess.board).to have_attributes(:squares => 'test')
     end
   end
-
-  describe "#create_squares" do
-    it 'creates the vitual squares of the chess_board' do
-      expect(board.create_squares).to include("squareG8"=>"\e[0;39;46m    \e[0m")
-    end
-  end
-end
+end 
