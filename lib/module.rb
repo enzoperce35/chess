b6d27a43a64c7c3d411c7efa003ba9b0065f033b
enzoperce35 <enzoperce35@gmail.com>
2021-01-09 07:59:57 +0800

    deleted

--- lib/module.rb
+++
@@ -1,8 +0,0 @@
-
-module ChessParts
-  PIECE_SET = {bishop: " \u265D  ", knight: " \u265E  ", rook:" \u265C  ", king: " \u265A  ", queen:" \u265B  ",  pawn: " \u265F  "}
-
-  ALPHA = ('A'..'H').to_a
-
-  X_COORDINATES = '   a    b   c   d   e   f   g   h'
-end
