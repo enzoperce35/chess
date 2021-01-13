1f0eb125a8291d64a5ccc3be3b06b65cb78de1e6
enzoperce35 <enzoperce35@gmail.com>
2021-01-14 06:34:56 +0800

    modified to colorize opposing player's pieces here

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,16 +1,20 @@
 require_relative 'chess_parts.rb'
+require 'colorize'
 
 class Pieces
   include ChessParts
 
+  #create each players' chess pieces with their attributes in a hash
   def create_pieces(piece_color, set = Hash.new)
-
     PIECE_SET.each do |piece,attr|
       image, row, count, alpha = attr.values
 
-      row = (9 - row) if piece_color == 'black'
+      if piece_color == 'black'
+        row = (9 - row)
 
-      store_piece(piece, image, row, count, alpha, set)
+        image = colorize_piece(image)
+      end
+    store_piece(piece, image, row, count, alpha, set)
     end
     set
   end
@@ -21,5 +25,9 @@ class Pieces
 
       set["#{piece}#{i+1}"] = { 'image'=>image, 'position'=>"#{position}#{row}", 'moves'=>[] }
     end
+  end
+
+  def colorize_piece(image)
+    image.colorize(color: :black)
   end
 end
