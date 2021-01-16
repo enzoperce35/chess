2f236b8757179dd3da9dedb66801ebb1584ad638
enzoperce35 <enzoperce35@gmail.com>
2021-01-17 02:55:09 +0800

    working interface for alphabet chosen active pieces side messages

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,16 +1,22 @@
 module ConsoleInterface
 
   #ask and get the player's name
-  def prompt_name(piece_color)
+  def prompt_name_interface(piece_color)
     puts "Who's Playing #{piece_color}?"
 
     gets.chomp!
   end
 
   #ask and the player's piece of choice
-  def select_piece(player)
+  def select_piece_interface(player, string)   #working, but needs cleaning
+    pieces = string.join.scan(/(\w\. \w+ \w\d)/)
+
+    string = string.join.scan(/(\w\.)/).map { |x| x[0].tr('.', '') }.sort { |a,b| a <=> b }
+
     puts "#{player}'s turn, please select your chess piece"
 
-    gets.chomp!
+    piece = gets.chomp! until string.include?(piece)
+
+    pieces.each { |x| return x[0].scan(/\w\d/).join if x[0].include?(piece + '. ') }
   end
 end
