203d38a6f5992e0acbda6a007321d513db1f203a
enzoperce35 <enzoperce35@gmail.com>
2021-01-27 18:16:01 +0800

    added possible_moves_side_message

--- lib/side_message.rb
+++ lib/side_message.rb
@@ -1,4 +1,30 @@
 module SideMessage
+  def create_message_lines(list, counter = 0)
+    arr = Array.new(6) { Array.new }
+
+    list.each do |item|
+      counter = 0 if counter == 6
+
+      arr[counter] << item.ljust(13)
+
+      counter += 1
+    end
+    arr.map! { |i| i.join.rstrip }
+  end
+
+  def create_list_items(pieces, arr = [])
+    pieces.each do |key,value|
+        piece = "   " + key.tr("0-9", "") + " #{value['position']}"
+        arr << piece
+    end
+    arr
+  end
+
+  def modify(piece_moves)
+    piece_moves.map! { |move| "   " + move}
+
+    piece_moves
+  end
 
   def active_pieces_side_message(player)
     piece_color = player.piece_color
@@ -12,24 +38,16 @@ module SideMessage
     upper_line + body_lines
   end
 
-  def create_list_items(pieces, arr = [])
-    pieces.each do |key,value|
-        piece = "   " + key.tr("0-9", "") + " #{value['position']}"
-        arr << piece
-    end
-    arr
-  end
+  def possible_moves_side_message(piece)
+    piece_name = piece['name']
+    piece_position = piece['position']
+    piece_moves = piece['moves']
 
-  def create_message_lines(list, counter = 0)
-    arr = Array.new(6) { Array.new }
+    items = modify(piece_moves)
 
-    list.each do |item|
-        counter = 0 if counter == 6
+    upper_line = ["'#{piece_name.upcase} #{piece_position.upcase}' MOVES".rjust(26)]
+    body_lines = create_message_lines(piece_moves)
 
-        arr[counter] << item.ljust(13)
-
-        counter += 1
-    end
-    arr.map! { |i| i.join.rstrip }
+    upper_line + body_lines
   end
 end
