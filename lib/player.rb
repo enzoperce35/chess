17d54558ded75f1450c313e1300f5a86e3ea4755
enzoperce35 <enzoperce35@gmail.com>
2021-02-08 06:52:50 +0800

    rubocop cleared

--- lib/player.rb
+++ lib/player.rb
@@ -1,44 +1,38 @@
 require_relative 'pieces.rb'
 require_relative 'interface.rb'
 
+# creates and modify the players and players' attributes
 class Player
-  attr_accessor :iteration, :player_a, :player_b, :piece_color
+  attr_accessor :iteration, :name, :piece_color, :piece_set
 
-  #returns a set of chess pieces
-  def get_piece_set
-    Pieces.new(piece_color).create_piece_set
+  # returns a set of chess pieces
+  def create_piece_set_for_the_player
+    @piece_set = Pieces.new(piece_color).create_piece_set
   end
 
-  #returns the name of the player
-  def ask_player_name
-    #ConsoleInterface.new.prompt_name_interface(piece_color)
-    iteration.zero? ? 'John' : 'Mark'  #not included
+  # returns the name of the player
+  def ask_for_player_name
+    # ConsoleInterface.new.prompt_name_interface(piece_color)
+    @name = iteration.zero? ? 'John' : 'Mark' # not included
   end
 
-  #returns the color of pieces to be created
-  def assign_piece_color
+  # returns the color of pieces to be created
+  def assign_color_to_the_player
     @piece_color = iteration.zero? ? 'white' : 'black'
   end
 
-  #returns the player assigned with a hash that will contain player values
-  def get_player
-    iteration.zero? ? @player_a = {} : @player_b = {}
-  end
-
-  #returns an array of players with a hash of player values
+  # returns an array of players with a hash of player attributes
   def create_players(players = [])
     2.times do |i|
       @iteration = i
 
-      player = get_player
+      ask_for_player_name
 
-      color = assign_piece_color
+      assign_color_to_the_player
 
-      name = ask_player_name
+      create_piece_set_for_the_player
 
-      piece_set = get_piece_set
-
-      players << { 'piece_color'=>color, 'name'=>name, 'active_pieces'=>piece_set }
+      players << { 'piece_color' => piece_color, 'name' => name, 'active_pieces' => piece_set }
     end
     players
   end
