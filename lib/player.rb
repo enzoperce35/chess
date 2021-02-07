2735f34cfa07866800d9ab710d7ecdba9bc47479
enzoperce35 <enzoperce35@gmail.com>
2021-02-08 07:09:48 +0800

    rubocop cleared

--- lib/player.rb
+++ lib/player.rb
@@ -3,25 +3,25 @@ require_relative 'interface.rb'
 
 # creates and modify the players and players' attributes
 class Player
-  attr_accessor :iteration, :name, :piece_color, :piece_set
+  attr_accessor :iteration, :player_name, :piece_color, :piece_set
 
-  # returns a set of chess pieces
+  # gets a set of chess pieces
   def create_piece_set_for_the_player
     @piece_set = Pieces.new(piece_color).create_piece_set
   end
 
-  # returns the name of the player
+  # gets the name of the player
   def ask_for_player_name
     # ConsoleInterface.new.prompt_name_interface(piece_color)
-    @name = iteration.zero? ? 'John' : 'Mark' # not included
+    @player_name = iteration.zero? ? 'John' : 'Mark' # not included
   end
 
-  # returns the color of pieces to be created
+  # gets the color for the pieces
   def assign_color_to_the_player
     @piece_color = iteration.zero? ? 'white' : 'black'
   end
 
-  # returns an array of players with a hash of player attributes
+  # returns an array of players with their attributes
   def create_players(players = [])
     2.times do |i|
       @iteration = i
@@ -32,7 +32,7 @@ class Player
 
       create_piece_set_for_the_player
 
-      players << { 'piece_color' => piece_color, 'name' => name, 'active_pieces' => piece_set }
+      players << { 'name' => player_name, 'piece_color' => piece_color, 'active_pieces' => piece_set }
     end
     players
   end
